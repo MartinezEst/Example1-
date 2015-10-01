92760e0026727a3ec12d6396870e9a9e905a6c6c
MartinezEst <martinez966@gmail.com>
2015-10-01 17:29:55 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Heihei git example1 siinpool!");
+			System.out.println("why is this shit so complicated?");
 	}
 
 }
